9b619ac65f0d49e2b74461c8ee417edc541731d4
Valeriy Golyshev <valeriy1golyshev@gmail.com>
2025-05-18 22:57:16 +0300

    registration: screen layout

--- lib/ui/registration/widgets/registration_screen.dart
+++ lib/ui/registration/widgets/registration_screen.dart
@@ -1,22 +1,79 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:life_calendar2/ui/core/themes/onboarding_theme.dart';
 import 'package:life_calendar2/ui/registration/bloc/registration_cubit.dart';
 
-class RegistrationScreen extends StatelessWidget {
+class RegistrationScreen extends StatefulWidget {
   const RegistrationScreen({super.key});
 
   @override
+  State<RegistrationScreen> createState() => _RegistrationScreenState();
+}
+
+class _RegistrationScreenState extends State<RegistrationScreen> {
+  final _birthdayTextController = TextEditingController();
+  final _lifeSpanTextController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    final brightness = Theme.of(context).brightness;
+    final onboardingTheme = OnboardingTheme(brightness: brightness);
     return BlocProvider(
       create: (context) => RegistrationCubit(authRepository: context.read()),
-      child: SafeArea(
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Column(
-              children: [
-                Image.asset('assets/calendar_splash_image.png'),
-                const TextField(),
-              ],
+      child: AnnotatedRegion(
+        value: SystemUiOverlayStyle(
+          statusBarColor: onboardingTheme.statusBarColor,
+          systemNavigationBarColor: onboardingTheme.systemNavigationBarColor,
+        ),
+        child: SafeArea(
+          child: Scaffold(
+            body: DecoratedBox(
+              decoration: BoxDecoration(gradient: onboardingTheme.gradient),
+              child: Column(
+                children: [
+                  Image.asset('assets/calendar_splash_image.png'),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16),
+                      child: Form(
+                        child: Column(
+                          children: [
+                            TextFormField(
+                              controller: _birthdayTextController,
+                              decoration: const InputDecoration(
+                                labelText: 'Введите дату рождения',
+                              ),
+                              onTapOutside:
+                                  (event) =>
+                                      FocusManager.instance.primaryFocus
+                                          ?.unfocus(),
+                            ),
+                            const SizedBox(height: 16),
+                            TextFormField(
+                              controller: _lifeSpanTextController,
+                              decoration: const InputDecoration(
+                                labelText: 'Продолжительность жизни',
+                              ),
+                              onTapOutside:
+                                  (event) =>
+                                      FocusManager.instance.primaryFocus
+                                          ?.unfocus(),
+                            ),
+                            Padding(
+                              padding: const EdgeInsets.all(48),
+                              child: OutlinedButton(
+                                onPressed: () {},
+                                child: const Text('Готово'),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
