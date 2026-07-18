7514b1997f43796adfed2474af17cb1d23b4ff45
Valeriy Golyshev <valeriy1golyshev@gmail.com>
2026-07-18 06:53:44 +0300

    Upgrade AGP

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ plugins {
 android {
     namespace = "com.vgol.life_calendar"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
